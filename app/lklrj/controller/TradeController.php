148031f511320a024cc73ab8ccef341e809ff74f
Mac.xiaojiu <274276153@qq.com>
2018-03-31 15:50:16 +0800

    小九
    交易记录

--- app/lklrj/controller/TradeController.php
+++ app/lklrj/controller/TradeController.php
@@ -10,12 +10,20 @@ namespace app\lklrj\controller;
 
 
 use app\admin\model\TradeModel;
+use app\admin\service\TerminaService;
 
 class TradeController extends BaseController
 {
     public function index(){
-        $agent = session('lkl_user')['org_code'];
-        $list = TradeModel::tb()->where(['org_code'=>$agent])->paginate(10);
+        $uid = session('user')['id'];
+
+        $tList = TerminaService::getListByUid($uid,1);
+        foreach ($tList as $v){
+            $tArray[] = $v['code'];
+        }
+        $where['term_no'] = ['IN',$tArray];
+        $list = TradeModel::tb()->where($where)->paginate(10);
+        
         // 获取分页显示
         $page = $list->render();
 
