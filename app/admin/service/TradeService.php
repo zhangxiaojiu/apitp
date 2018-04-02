8ade5fbe08d1fd387ca28e01268ab24e53c1a3f9
Mac.xiaojiu <274276153@qq.com>
2018-04-02 15:16:46 +0800

    小九-empty arr
    $terminalArray

--- app/admin/service/TradeService.php
+++ app/admin/service/TradeService.php
@@ -20,19 +20,25 @@ class TradeService
      */
     public static function getTradeTotal($id,$startDate,$endDate){
         $terminalList = TerminaService::getListByUid($id);
-        foreach($terminalList as $v){
-            $terminalArray[] = $v['code'];
+        if(!empty($terminalList)) {
+            foreach ($terminalList as $v) {
+                $terminalArray[] = $v['code'];
+            }
+            $where['term_no'] = ['IN', $terminalArray];
+            $where['trans_time'] = ['between', "$startDate,$endDate"];
+            $tradeList = TradeModel::tb()->where($where)->select();
+            $amt = 0;
+            foreach ($tradeList as $v) {
+                $amt += $v['trans_amt'];
+            }
+            $total['num'] = count($tradeList);
+            $total['amt'] = $amt;
+            return $total;
+        }else{
+            $total['num'] = 0;
+            $total['amt'] = 0;
+            return $total;
         }
-        $where['term_no'] = ['IN',$terminalArray];
-        $where['trans_time'] = ['between',"$startDate,$endDate"];
-        $tradeList = TradeModel::tb()->where($where)->select();
-        $amt = 0;
-        foreach ($tradeList as $v){
-            $amt += $v['trans_amt'];
-        }
-        $total['num'] = count($tradeList);
-        $total['amt'] = $amt;
-        return $total;
     }
 
 
